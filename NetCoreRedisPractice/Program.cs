c1fce7f58cce3a0e1c79ba4615a1a5477bc6e8b7
aillieo <aillieo@163.com>
2021-01-22 21:31:42 +0800

    first commit

---
+++ NetCoreRedisPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace NetCoreRedisPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379"))
+            {
+                IDatabase db = redis.GetDatabase();
+                string value = "myvalue";
+                db.StringSet("mykey", value);
+                Console.WriteLine(db.StringGet("mykey"));
+
+                ISubscriber sub = redis.GetSubscriber();
+                sub.Subscribe("messages", (channel, message) => {
+                    Console.WriteLine((string)message);
+                });
+                string messages = "mymessages";
+                sub.Publish("messages", messages);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
